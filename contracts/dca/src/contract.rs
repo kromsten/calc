2980824fbe4413d129b487a558d557ea67a637e8
fluffydonkey <111311384+fluffydonkey@users.noreply.github.com>
2023-04-14 11:38:21 +1000

    version

--- contracts/dca/src/contract.rs
+++ contracts/dca/src/contract.rs
@@ -48,27 +48,6 @@ pub const AFTER_BANK_SWAP_REPLY_ID: u64 = 4;
 
 #[entry_point]
 pub fn migrate(deps: DepsMut, _env: Env, msg: MigrateMsg) -> Result<Response, ContractError> {
-    deps.api.addr_validate(&msg.admin.to_string())?;
-    deps.api
-        .addr_validate(&msg.staking_router_address.to_string())?;
-
-    assert_fee_collector_addresses_are_valid(deps.as_ref(), &msg.fee_collectors)?;
-    assert_fee_collector_allocations_add_up_to_one(&msg.fee_collectors)?;
-    assert_dca_plus_escrow_level_is_less_than_100_percent(msg.dca_plus_escrow_level)?;
-
-    update_config(
-        deps.storage,
-        Config {
-            admin: msg.admin.clone(),
-            fee_collectors: msg.fee_collectors,
-            swap_fee_percent: msg.swap_fee_percent,
-            delegation_fee_percent: msg.delegation_fee_percent,
-            staking_router_address: msg.staking_router_address,
-            page_limit: msg.page_limit,
-            paused: msg.paused,
-            dca_plus_escrow_level: msg.dca_plus_escrow_level,
-        },
-    )?;
 
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
 
